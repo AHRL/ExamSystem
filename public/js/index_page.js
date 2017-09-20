6613fcdfe37ecc658a183ce97a8b274a36b72652
swpuLeo <swpu.leo@gmail.com>
2017-09-20 11:48:53 +0800

    push index_page.js

---
+++ public/js/index_page.js
@@ -0,0 +1,14 @@
+var classSe = {
+    add: addClass
+};
+
+function addClass(obj, cl) {
+
+}
+
+window.onload = function () {
+    var h1Anim = document.getElementsByClassName('h1-anim'),
+        h2Anim = document.getElementsByClassName('h2-anim');
+
+
+};
