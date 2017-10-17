0859ad4aa8217891e9937f6c9fba2c2f9f01f872
hhh <957400829@qq.com>
2017-10-17 20:15:12 +0800

    add login details

--- src/main/java/com/iot/controller/ExamCtro.java
+++ src/main/java/com/iot/controller/ExamCtro.java
@@ -5,15 +5,13 @@ import com.iot.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
 
 /**
  * Created by xiongxiaoyu on 2017/10/9.
  */
-
 
 
 @Controller
@@ -22,15 +20,19 @@ public class ExamCtro{
     @Autowired
     private UserRepository userRepository;
 
+    @RequestMapping(value = {"/","/funExam"}, method = RequestMethod.GET)
+    public String index()
+    {return "funExam";}
 
-    @RequestMapping("/")
-    public String index(){return "login";}
+    @RequestMapping(value = "/login",method = RequestMethod.GET)
+    public String login()throws Exception{
+        return "login";
+    }
 
-//, @RequestParam(name = "upEmail")String email
 
-    @RequestMapping("/login")
-    public String login(HttpServletRequest request)
-            throws ServletException, IOException {
+    @RequestMapping(value = "/register",method = RequestMethod.GET)
+    public String register(HttpServletRequest request) throws  Exception {
+
         String Email=request.getParameter("upEmail");
         String Username=request.getParameter("upUsername");
         String Password=request.getParameter("upPassword");
@@ -38,15 +40,44 @@ public class ExamCtro{
         request.getSession().setAttribute("Email",Email);
         request.getSession().setAttribute("Username",Username);
         request.getSession().setAttribute("Password",Password);
-
 //        model.addAttribute("Email",Email);
 //        model.addAttribute("Username",Username);
 //        model.addAttribute("Password",Password);
-
         User user=new User(Email,Username,Password);
+        user.setRole(User.ROLE.user);
         userRepository.save(user);
         return "404";
-
     }
+
+
+//    @RequestMapping(value = {"/", "/testHome"}, method= RequestMethod.GET)
+//    public String home(){
+//        return "testHome";
+//    }
+//
+//    @RequestMapping(value = "/testAdmin", method=RequestMethod.GET)
+//    @PreAuthorize("hasAnyRole('admin')")
+//    public String testAdmin(){
+//        return "testAdmin";
+//    }
+//
+//    @RequestMapping(value = "/testUser", method=RequestMethod.GET)
+//    @PreAuthorize("hasAnyRole('user','admin')")
+//    public String testUser(){
+//        return "testUser";
+//    }
+//
+//    @RequestMapping(value = "/testLogin", method=RequestMethod.GET)
+//    public String login(){
+//        return "testLogin";
+//    }
+//
+//    @RequestMapping("/test403")
+//    public String forbidden(){
+//        return "test403";
+//    }
+//
+//
+
 
 }
--- src/main/java/com/iot/model/login/User.java
+++ src/main/java/com/iot/model/login/User.java
@@ -6,46 +6,73 @@ import javax.persistence.*;
  * Created by xiongxiaoyu on 2017/10/9.
  */
 
-
 @Entity
 @Table(name = "user")
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    public Long id;
+    public enum ROLE{
+    admin,user;
+    }
 
-    private String Email;
-    private String Username;
-    private String Password;
+    @Enumerated(EnumType.STRING)
+    private ROLE role;
+
+
+    @Id
+    private String username;
+
+    private String password;
+    private String email;
+
+    public ROLE getRole() {
+        return role;
+    }
+
+    public void setRole(ROLE role) {
+        this.role = role;
+    }
 
     public String getEmail() {
-        return Email;
+        return email;
     }
 
     public void setEmail(String email) {
-        this.Email = email;
+        this.email = email;
     }
 
     public String getUsername() {
-        return Username;
+        return username;
     }
 
     public void setUsername(String username) {
-        this.Username = username;
+        this.username = username;
     }
 
     public String getPassword() {
-        return Password;
+        return password;
     }
 
     public void setPassword(String password) {
-        this.Password = password;
+        this.password = password;
     }
 
-    public User(String email, String username, String password) {
-        Email = email;
-        Username = username;
-        Password = password;
+
+    public User() {
+    }
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "role=" + role +
+                ", email='" + email + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
     }
 }
